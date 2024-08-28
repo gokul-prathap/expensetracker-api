5b089962a69c552a8f6848af464692b61f197037
gokul-prathap <gokulprathap@rocketmail.com>
2024-08-28 17:14:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
+const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const express = require('express');
 const mongoose = require('mongoose');
 
 const expenseRoutes = require('./routes/expenseroutes');
 
 const app = express();
-// app.use(cors());
-app.use(cors({
-  origin: 'https://expensetracker-ui-self.vercel.app',
-}));
+app.use(cors());
 app.use(express.json());
 
 console.log(process.env.MONGO_URI);
