abc3e7e1ec52b8d0902e60d5874833a181ee4154
gokul-prathap <gokulprathap@rocketmail.com>
2025-01-15 19:15:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,16 +6,9 @@ const mongoose = require('mongoose');
 const expenseRoutes = require('./routes/expenseroutes');
 
 const app = express();
-// app.use(cors());
-app.use(cors({
-  origin: '*',
-  methods: 'GET, POST, PUT, DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
-}));
+app.use(cors());
 app.use(express.json());
 
-console.log(process.env.MONGO_URI);
-console.log('\n\n\n\n************************');
 
 mongoose.connect(process.env.MONGO_URI || '', {
     useNewUrlParser: true,
