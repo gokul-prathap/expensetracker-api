da477c65fa3351009b42c9e7901dca0b5172f45f
gokul-prathap <gokulprathap@rocketmail.com>
2025-02-07 20:36:00 +0530

    delete functionality added
     delete functionality added
    several other changes

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -21,10 +21,15 @@ exports.getAllExpensesForm = async (req, res) => {
     }
 };
 
-// exports.deleteExpense =  async (req,res) => {
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// }
+exports.deleteExpense = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedExpense = await Expense.findByIdAndDelete(id);
+        if (!deletedExpense) {
+            return res.status(404).json({ message: 'Expense not found' });
+        }
+        res.status(200).json({ message: 'Expense deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+};
--- src/routes/expenseroutes.js
+++ src/routes/expenseroutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { submitForm } = require('../controllers/expenseController');
 const { getAllExpensesForm } = require('../controllers/expenseController');
+const { deleteExpense } = require('../controllers/expenseController');
 const router = express.Router();
 
 router.post('/submit', submitForm);
-router.get('/expenses', getAllExpensesForm);
+router.get('/expenses/getAllExpenses', getAllExpensesForm);
 // router.put('/update', updateExpense)
-// router.delete('/delete', deleteExpense)
+router.delete('/expenses/deleteExpense/:id', deleteExpense);
 
 module.exports = router;
